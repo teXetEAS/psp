733b52d6f3de3744d1c6971ea967e32d000a703a
texet <texet1993texet@yandex.ru>
2020-07-12 11:26:40 +0400

    add log

--- log.py
+++ log.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import scan
 import os
 from pathlib import *
 from datetime import datetime
@@ -14,29 +13,11 @@ if not os.path.exists(path):
 	with open(path, "a") as logFile:
 		logFile.write("\t\t\t#####LOG_FILE#####\n")
 
-def logFile(ip):
-	scanIpMac = scan.scan(ip)		
-	scanMacList = []
-	for item in scanIpMac:		
-		scanMacList.append(item["mac"])
-	macList = []
-	with open(path, "r") as logFile:
-		for line in logFile:
-			if line.startswith("[ignor]"):
-				line = line.split()
-				macList.append(line[-1])
-	newMacList = list(set(scanMacList) - set(macList))
+def logScan(targetIp, targetMac):
+	with open(path, "a") as logFile:
+		logFile.write("\n[+] " + date + time + " " + targetIp + "/" + targetMac + "\n")
+		
+def logSniff(resalt):
+	with open(path, "a") as logFile:
+		logFile.write(resalt + "\n")
 	
-	
-	
-	if len(newMacList) > 0:
-		for item in newMacList:
-			userInput = str(input("Игнорировать хочт с MAC-адресом " + item["mac"] " (y/n)").lower())
-			if userInput == "y":
-				with open(path, "a") as logFile:
-					logFile.write("[ignore]" + date + time + 
-
-try:
-	logFile("192.168.1.1/24")
-except PermissionError:
-	print("Необходимы права Супер-пользователя")
